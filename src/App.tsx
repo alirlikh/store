c6c2a643159c78010c793a2e3694d5da71e9ac8d
Alireza Jalili <alirlikh@gmail.com>
2024-11-16 13:04:18 +0330

    feat: add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { Button } from "./components/ui/button"
+import { useAppSelector } from "./hooks"
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Orders,
+  Checkout,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Login,
+  Register
+} from "./pages"
 
 const App = () => {
   return (
     <>
-    <h1>app</h1>
-    <Button variant={"outline"} >Click me</Button>
+      <h1>app</h1>
+      <Button variant={"outline"}>Click me</Button>
     </>
   )
 }
