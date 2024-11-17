06cc4f4d8ce5bdb07eb7eb84211b122558d38c6d
Alireza Jalili <alirlikh@gmail.com>
2024-11-17 11:27:19 +0330

    feat: navbar links

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,7 +1,23 @@
-import React from "react"
+import { links } from "@/utils"
+import { NavLink } from "react-router-dom"
 
 const NavLinks = () => {
-  return <div>NavLinks</div>
+  return (
+    <div className="hidden lg:flex justify-center items-center gap-x-4">
+      {links.map((link) => {
+        return (
+          <NavLink
+            to={link.href}
+            className={(isActive) => {
+              return `capitalize font-light tracking-wide ${isActive ? "text-primary" : ""}`
+            }}
+          >
+            {link.label}
+          </NavLink>
+        )
+      })}
+    </div>
+  )
 }
 
 export default NavLinks
