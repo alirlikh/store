736cf7c8d6826f700aaf8f1d92702f0df754541b
Alireza Jalili <alirlikh@gmail.com>
2024-11-17 15:57:35 +0330

    feat: add cartbutton

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,7 +1,25 @@
-import React from "react"
+import { Link } from "react-router-dom"
+import { Button } from "./ui/button"
+import { ShoppingCart } from "lucide-react"
 
 const CartButton = () => {
-  return <div>CartButton</div>
+  const numItemsInCart = 5
+
+  return (
+    <Button
+      asChild
+      size={"icon"}
+      variant={"outline"}
+      className="flex justify-center items-center relative"
+    >
+      <Link to={"/cart"}>
+        <ShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full w-6 h-6 text-xs flex justify-center items-center">
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  )
 }
 
 export default CartButton
