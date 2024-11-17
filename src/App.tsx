062546640383a18db22d314196a4609a37e6138f
Alireza Jalili <alirlikh@gmail.com>
2024-11-17 16:15:40 +0330

    feat: 404 error

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    errorElement: <Error />,
     children: [
       { index: true, element: <Landing /> },
       { path: "products", element: <Products /> },
@@ -28,8 +29,8 @@ const router = createBrowserRouter([
       { path: "orders", element: <Orders /> }
     ]
   },
-  { path: "/login", element: <Login /> },
-  { path: "/register", element: <Register /> }
+  { path: "/login", element: <Login />, errorElement: <Error /> },
+  { path: "/register", element: <Register />, errorElement: <Error /> }
 ])
 
 const App = () => {
