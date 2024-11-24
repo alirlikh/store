6435b038bd5f29793890820f47b587458f7bc81e
Alireza Jalili <alirlikh@gmail.com>
2024-11-24 09:59:28 +0330

    feat: create forminput componenet

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,18 +1,14 @@
 import { Form, useLoaderData, Link } from "react-router-dom"
-import { Label } from "./ui/label"
 import { Button } from "./ui/button"
-import { Input } from "./ui/input"
+import FormInput from "./FormInput"
 import { ProductsResponseWithParams } from "@/utils"
 
 const Filters = () => {
   const { meta, params } = useLoaderData() as ProductsResponseWithParams
-  const { search } = params
+  const { search, company, category, shipping, order, price } = params
   return (
     <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-      <div className="mb-2">
-        <Label htmlFor="search">Search Product</Label>
-        <Input id="search" name="search" type="text" defaultValue={search} />
-      </div>
+      <FormInput type="search" label="search product" name="search" defaultValue={search} />
       <Button type="submit" size="sm" className="self-end mb-2">
         search
       </Button>
