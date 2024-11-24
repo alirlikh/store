c9eb9ad0007794dfc49d9d9096553b98a34c9d6d
Alireza Jalili <alirlikh@gmail.com>
2024-11-24 10:31:30 +0330

    feat: create formselect component

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,6 +1,7 @@
 import { Form, useLoaderData, Link } from "react-router-dom"
 import { Button } from "./ui/button"
 import FormInput from "./FormInput"
+import FormSelect from "./FormSelect"
 import { ProductsResponseWithParams } from "@/utils"
 
 const Filters = () => {
@@ -9,6 +10,25 @@ const Filters = () => {
   return (
     <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       <FormInput type="search" label="search product" name="search" defaultValue={search} />
+      <FormSelect
+        label="select category"
+        name="category"
+        options={meta.categories}
+        defaultValue={category}
+      />
+      <FormSelect
+        label="select company"
+        name="company"
+        options={meta.companies}
+        defaultValue={company}
+      />
+      <FormSelect
+        label="order by"
+        name="order"
+        options={["a-z", "z-a", "high", "low"]}
+        defaultValue={order}
+      />
+
       <Button type="submit" size="sm" className="self-end mb-2">
         search
       </Button>
