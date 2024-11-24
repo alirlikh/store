ec3de278c4a616c5289f338ec01765effd49645b
Alireza Jalili <alirlikh@gmail.com>
2024-11-24 09:47:36 +0330

    feat: add params to product page and filter them

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,14 +1,15 @@
 import { Filters, PaginationContainer, ProductsContainer } from "@/components"
-import { customFetch, type ProductsResponse } from "@/utils"
+import { customFetch, type ProductsResponse, type ProductsResponseWithParams } from "@/utils"
 import { LoaderFunction } from "react-router-dom"
 
 const url = "/products"
 
-export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
-  const response = await customFetch<ProductsResponse>(url)
-  console.log(response)
+export const loader: LoaderFunction = async ({ request }): Promise<ProductsResponseWithParams> => {
+  const params = Object.fromEntries([...new URL(request.url).searchParams.entries()])
 
-  return { ...response.data }
+  const response = await customFetch<ProductsResponse>(url, { params })
+
+  return { ...response.data, params }
 }
 
 const Products = () => {
