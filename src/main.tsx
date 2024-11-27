4e1dafb85f425ad86a9cbc6003353084f06e5024
Alireza Jalili <alirlikh@gmail.com>
2024-11-27 15:12:53 +0330

    feat: add toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Provider } from 'react-redux'
-import { store } from './store.ts'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import { Provider } from "react-redux"
+import { store } from "./store.ts"
+import { Toaster } from "./components/ui/toaster.tsx"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <Toaster />
+      <App />
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
