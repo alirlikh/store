7118bfcb6be41572febad5d03390a354f49d4b39
Alireza Jalili <alirlikh@gmail.com>
2024-12-02 11:01:02 +0330

    fix: demo user changed to dynamic user

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
-import { useNavigate, Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { Button } from "./ui/button"
-import { useState } from "react"
+import { useAppDispatch, useAppSelector } from "@/hooks"
+
+import { logoutUser } from "../features/user/userSlice"
+import { clearCart } from "../features/cart/cartSlice"
+import { useToast } from "@/hooks/use-toast"
 
 const Header = () => {
   const navigate = useNavigate()
-  const [user, setUser] = useState<{ username: string } | null>({ username: "demo user" })
+  const dispatch = useAppDispatch()
+  const { toast } = useToast()
+  const user = useAppSelector((state) => state.userState.user)
 
   const handleLogout = () => {
-    setUser(null)
+    dispatch(clearCart())
+    dispatch(logoutUser())
+    toast({ description: "Logged out" })
     navigate("/")
   }
 
